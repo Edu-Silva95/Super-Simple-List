0fde9ef2831cff4a14bd098704ff76e2c31ccbbc
Eduardo Silva <joaomalta4@hotmail.com>
2026-03-30 16:15:08 +0100

    Fixing a mobile styling issue with the color of the items inside the list

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5000",
-        changeOrigin: true,
-      },
-    },
     watch: {
       usePolling: true,
       interval: 100,
